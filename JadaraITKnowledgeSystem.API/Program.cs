1c762e45eee037ae3357f1127a3b5fb39c31151e
muhannadlahlouh <muhannadlahlouh2004@gmail.com>
2025-11-05 21:30:40 +0300

    adding conection string

--- JadaraITKnowledgeSystem.API/Program.cs
+++ JadaraITKnowledgeSystem.API/Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SecondConnection")));
 
 // Register Storage Service
 builder.Services.AddHttpClient<IStorageService, BunnyStorageService>();
